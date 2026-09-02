3c327ec1dab0e15fa16094fc263581f2c5766bbc
fryzzfabrizo-sys <fryzzfabrizo@gmail.com>
2026-09-03 00:16:00 +0300

    Reorder function declarations in silent.h

--- sources/silent.h
+++ sources/silent.h
@@ -1,5 +1,4 @@
 #pragma once
-
-void InitSilentAimThread();   // запустить поток один раз
-void RunSilentAim();          // вызывается каждый кадр, обновляет данные для потока
-void ResetSilentAim();        // заглушка (опционально)
+void RunSilentAim();
+void ResetSilentAim();
+void InitSilentAimThread();
