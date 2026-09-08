047a52db265c61ca18ab8f19dfa31e45fa2e7939
fryzzfabrizo-sys <fryzzfabrizo@gmail.com>
2026-09-09 01:27:17 +0300

    Update silent.mm

--- sources/silent.mm
+++ sources/silent.mm
@@ -1,6 +1,6 @@
 #import "../esp/Core/GameLogic.h"
 #import "../esp/drawing_view/esp.h"
-#import "../drawing_view/offset.h"
+#import "../esp/drawing_view/offset.h"
 #import "mahoa.h"
 #include <cmath>
 #include <atomic>
